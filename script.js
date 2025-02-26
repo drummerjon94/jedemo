1b2c6ecd470e3227f03029893f36ca7123281292
drummerjon94 <jonny-evans@outlook.com>
2025-02-26 23:43:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById('certificateForm').addEventListener('submit', function (
   // Update certificate content
   document.getElementById('certificateName').textContent = `This certificate is awarded to ${name}`;
   document.getElementById('certificateMessage').textContent = message;
+  document.getElementById('certificateScreenshot').textContent = 'Screenshot and send to the Whatsapp group;
 
   // Show the certificate
   document.getElementById('certificate').classList.remove('hidden');
